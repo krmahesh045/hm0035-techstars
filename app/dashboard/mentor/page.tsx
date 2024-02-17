e176227d3703a13cf710f2bb17a95ab2c63260d1
Swapnil Kapale <swapnilkapaleofficial@gmail.com>
2024-02-18 04:46:02 +0530

    eslint disabled

--- app/dashboard/mentor/page.tsx
+++ app/dashboard/mentor/page.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable*/
 
 // MentorDetailPage.tsx
 
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
-
+/* eslint-disable*/
 'use client'
 import React from 'react'
 import { useRouter } from 'next/navigation'
