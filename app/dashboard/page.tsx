f70b3378837663ce5334bdf42fb5b8b77ce120b2
Swapnil Kapale <swapnilkapaleofficial@gmail.com>
2024-02-22 12:31:58 +0530

    logout working

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import Card from './card'
 import Link from 'next/link'
 import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 
 type Props = {}
@@ -96,14 +97,19 @@ const DashboardPage = (props: Props) => {
 
   ];
 
+  const router = useRouter();
+
   // Logout function
   async function handleLogout() {
+
+    console.log("Logging out");
     const response =  await axios.get('/api/logout');
     if(response.status !== 200) {
       console.error("Error logging out:", response);
     }
     else {
       console.log("Logged out successfully");
+      router.push('/signin');
     }
   }
 
@@ -126,14 +132,16 @@ const DashboardPage = (props: Props) => {
 
         <div className='flex gap-8 ml-auto'>
           {/* Filter icon */}
-          <button className='flex gap-4' onClick={handleLogout}>
+          <button 
+            className='flex gap-4' 
+          >
             <svg width="36" height="36" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill="currentColor" d="m8.398 14.605l1.323 1.143c.29.251.323.691.075.984a.688.688 0 0 1-.976.075l-1.565-1.352a.7.7 0 0 1-.242-.53V7.938L1.171 1.155C.78.703 1.1 0 1.694 0h16.612c.594 0 .912.704.523 1.155l-5.85 6.784v11.363c0 .386-.31.698-.692.698a.695.695 0 0 1-.692-.698V7.678a.7.7 0 0 1 .17-.458l5.023-5.825H3.21L8.228 7.22a.7.7 0 0 1 .17.458z" />
             </svg>
           </button>
 
           {/* Logout icon */}
-          <button className='flex gap-2 items-center mr-4'>
+          <button className='flex gap-2 items-center mr-4' onClick={handleLogout}>
             <svg width="36" height="36" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2">
                 <path stroke-dasharray="32" stroke-dashoffset="32" d="M12 4H5C4.44772 4 4 4.44772 4 5V19C4 19.5523 4.44772 20 5 20H12">
