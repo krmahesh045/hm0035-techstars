75a49e3992ba0d70ef3b1dfb7246e84d1a4ace6a
Swapnil Kapale <swapnilkapaleofficial@gmail.com>
2024-02-23 00:43:20 +0530

    mentorprofile working

--- app/(dashboard)/student/[mentorProfile]/page.tsx
+++ app/(dashboard)/student/[mentorProfile]/page.tsx
@@ -21,22 +21,57 @@ type feedback = {
   stars: number;
 }
 
+export interface MentorData {
+  user: User
+  mentor: Mentor
+}
+
+export interface User {
+  _id: string
+  name: string
+  email: string
+  phone: string
+  address: string
+}
+
+export interface Mentor {
+  _id: string
+  userId: string
+  experties: string
+  skills: string[]
+  qualification: string
+  about: string
+  udemy: string
+  linkedin: string
+  youtube: string
+}
+
 
 
 const MentorDetailPage: FC<MentorProfileProps> = ({params}) => {
   // Assuming you have mentor data
 
   const [mentorId, setMentorId] = useState(params.mentorProfile) 
-  const [mentorData, setMentorData] = useState({
-    name: '',
-    role: '',
-    skills: [],
-    qualification: '',
-    feedback: [],
-    phone: '',
-    email: '',
-    about: ''
-  });
+  const [mentorData, setMentorData] = useState<MentorData>({
+    user: {
+      _id: '',
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+    },
+    mentor: {
+      _id: '',
+      userId: '',
+      experties: '',
+      skills: [],
+      qualification: '',
+      about: '',
+      udemy: '',
+      linkedin: '',
+      youtube: '',
+    }
+  } as MentorData)
 
   const fetchProfile = async () => {
     // fetch mentor data
@@ -45,8 +80,9 @@ const MentorDetailPage: FC<MentorProfileProps> = ({params}) => {
     }
     console.log(data.userId);
     const response = await axios.post('/api/mentorProfile', data);
-    console.log(response.data); 
-    setMentorData(response.data.mentor);
+    console.log(response.data.user); 
+    console.log(response.data.mentor);
+    setMentorData(response.data);
   };
 
   useEffect(() => {
@@ -86,10 +122,10 @@ const MentorDetailPage: FC<MentorProfileProps> = ({params}) => {
  
         <div className='h-full flex flex-col justify-center mt-10 gap-10'>
           <h1 className='text-7xl font-semibold text-white ml-[400px] '>
-            {mentorData.name}
+            {mentorData.user.name}
           </h1>
           <h1 className='text-xl font-semibold text-white ml-[400px] '>
-            {mentorData.role}
+            {mentorData.mentor.experties}
           </h1>
         </div>
 
@@ -101,7 +137,7 @@ const MentorDetailPage: FC<MentorProfileProps> = ({params}) => {
         <div className='flex flex-col gap-4'>
           <h1 className='text-3xl font-semibold'>Skills</h1>
           <div className='flex gap-4'>
-            {mentorData.skills.map((skill, index) => (
+            {mentorData.mentor.skills.map((skill, index) => (
               <h1 key={index} className='bg-violet-100 rounded-xl p-2 text-zinc-900'>
                 {skill}
               </h1>
@@ -110,20 +146,15 @@ const MentorDetailPage: FC<MentorProfileProps> = ({params}) => {
         </div>
         <div className='flex flex-col gap-4'>
           <h1 className='text-3xl font-semibold'>Qualification</h1>
-          <h1 className='text-lg'>{mentorData.qualification}</h1>
+          <h1 className='text-lg'>{mentorData.mentor.qualification}</h1>
         </div>
-        <div className='flex flex-col gap-4'>
-          <h1 className='text-3xl font-semibold'>Feedback</h1>
-          {mentorData.feedback.map((feedback, index) => (
-            <h1 key={index} className='text-lg'>{feedback}</h1>
-          ))}
-        </div>
+      
 
         {/* contacts */}
         <div className='flex flex-col gap-4'>
           <h1 className='text-3xl font-semibold'>Contact</h1>
-          <h1 className='text-lg'>{mentorData.phone}</h1>
-          <h1 className='text-lg'>{mentorData.email}</h1>
+          <h1 className='text-lg'>{mentorData.user.phone}</h1>
+          <h1 className='text-lg'>{mentorData.user.email}</h1>
         </div>
       </div>
       
@@ -133,8 +164,7 @@ const MentorDetailPage: FC<MentorProfileProps> = ({params}) => {
         <div className='w-1/2'>
           <h1 className='text-3xl font-semibold mb-10'>About</h1>
           <p className='text-lg'>
-          I am an accomplished React and Node.js developer with a solid background in JavaScript and a Bachelors degree in Computer Science. I am passionate about mentoring and sharing my expertise to help others excel in their development journey. Feel free to reach out to me for any inquiries or collaboration opportunities.
-
+            {mentorData.mentor.about}
           </p>
         </div>
         
@@ -172,8 +202,7 @@ const MentorDetailPage: FC<MentorProfileProps> = ({params}) => {
                   </div>
                 </div>
               ))}
-              
-          
+                  
             </div>
         </div>
       </div>
