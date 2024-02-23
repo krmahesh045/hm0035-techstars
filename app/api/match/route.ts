9268afc1e9b7a21e6808ea2f094ced6788fad620
manojjamble <manoj.jamble21@pccoepune.org>
2024-02-23 06:54:43 +0530

    updated ml model api

--- app/api/match/route.ts
+++ app/api/match/route.ts
@@ -1,5 +1,3 @@
-// pages/api/match/route.js
-
 import connectMongoDB from '@/libs/mongodb';
 import Student from '@/models/student';
 import Mentor from '@/models/mentor';
@@ -11,19 +9,35 @@ export async function GET(request: NextRequest) {
   await connectMongoDB();
 
   try {
-    const {mentorId} = await request.json();
-    const {userId} = getDataFromToken(request);
+    const { mentorId } = await request.json();
+    const { userId } = getDataFromToken(request);
 
-    const studentsArray = await Student.find({userId}).select('userId testData');
-    const mentorsArray = await Mentor.find({userId: mentorId}).select('userId testData');
-   return  NextResponse.json({
-      message : "Success",
-      students: studentsArray,
-      mentors: mentorsArray,
+    const studentsArray = await Student.find({ userId }).select('userId testData');
+    const mentorsArray = await Mentor.find({ userId: mentorId }).select('userId testData');
+
+    // Prepare data to send to external API
+    const requestData = {
+      studentsArray,
+      mentorsArray, // Corrected key name
+    };
+
+    // Call the external API
+    const response = await fetch('http://localhost:3001/calculate-similarity', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
     });
+
+    // Parse the response from the external API
+    const responseData = await response.json();
+    console.log(responseData);
+    // Return the response from the external API
+    return NextResponse.json(responseData);
 
   } catch (error) {
     console.error(error);
-    NextResponse.json({ error: 'Internal Server Error' });
+    return NextResponse.json({ error: 'Internal Server Error' });
   }
 }
